0d7f3142ca38fffa55c6a6ccea57aa7f7b726672
Matthew Cole <mcole8@binghamton.edu>
2016-11-10 09:49:59 -0500

    Started register file class
    Began coding the register class; updated driver program to remove
    functionality not required by project (per discussion with Prof. Ghose)

--- main.cpp
+++ main.cpp
@@ -12,25 +12,24 @@ using namespace std;
 //Simulator variables with external linkage
 extern int cycle = 0; //simulator's current cycle
 extern const char* instFile; //instruction input file
-extern const char* memFile; //memory input/output file
 
 // Initialize the simulator to a known state.
-// Optionally <format> the memory text file.
 void initialize()
 {
   cout << "initialize()" << endl;
-  //TODO Create instance of the MMU
-  //TODO Create instance of the Register File
-  //TO DO Create instances of the CPU function units
+  //TODO Call to data::initialize()
+  //TODO Call to registers::initialize()
+  //TODO Call to cpu::initialize()
 }
 
 // Display the simulator internal state.
-void display(int num_addrs=0)
+void display(int start=0, int stop=0)
 {
   cout << "display()" << endl;
   //TODO Call to cpu::display()
-  //TODO Call to register::display()
-  //TODO Call to memory::display(num_addrs)
+  //TODO Call to registers::display()
+  //TODO Call to data::display(start,stop)
+  //TODO Call to code::display()
 }
 
 // Simulate the operation of the system for <num_cycles> or until a HALT instruction is encountered. Optionally display the simulator internal state every <disp_cycles> cycles. If <disp_cycles> is zero, the internal state is not displayed.
@@ -45,7 +44,7 @@ void simulate(int num_cycles, unsigned int disp_cycles=0)
     cout << "simulate()" << endl;
     //if(!cpu::simulate()) // simulate returns 0 if execution should not continue (EOF or HALT encountered)
     //  break;
-    
+
     //Determine whether the machine state should be displayed
     if (display && ((c - start) % display == 0))
       display();
@@ -73,7 +72,7 @@ void interface(string command, int n=1, int d=0)
     case "h": //falls through to default (which prints the help message)
     default:
       cout << "[i]                        (I)nitialize the simulator state" << endl;
-      cout << "[s num_cycles disp_cycles] (S)imulate the number of cycles specified by <n>," << endl; 
+      cout << "[s num_cycles disp_cycles] (S)imulate the number of cycles specified by <n>," << endl;
       cout << "                               displaying contents every <disp_cycles> cycles" << endl;
       cout << "[d num_addr]               (D)isplay the contents of each stage in the pipeline," << endl;
       cout << "                               registers/flags, and the first <num_addr> memory " <<endl;
@@ -88,36 +87,31 @@ int main(int argc, char** argv)
 {
   //Command line program options handling
   //Check if user needs a usage message
-  if (argc != 3) {
-    cout << "USAGE: apex-sim [-h|--help][[-q|--quiet]|[-v|--verbose] <instructions file> <memory file>" << endl;
+  if (argc == 1) {
+    cout << "USAGE: apex-sim [-h|--help] <instructions file>" << endl;
     exit(0);
   }
-  else {
-    for (int a = 1; a < argc; a++) {
-      //Note: strcmp returns 0 on match, so it must be negated
-      if (!strcmp(argv[a], "-h") || !strcmp(argv[a],"--help")) {
-        cout << "USAGE: apex-sim [-h|--help] <instructions file> <memory file>" << endl;
+  else if (!strcmp(argv[1], "-h") || !strcmp(argv[1],"--help")) {
+        cout << "USAGE: apex-sim [-h|--help] <instructions file>" << endl;
         exit(0);
       }
     }
   }
-  
+
   //User didn't need a usage message. Capture filenames
   instFile = (const char*) argv[1];
-  memFile = (const char*) argv[2];
-  
-  //Instantiate Simulator classes
-  /**** TODO ****/
-  
-  //Simulator Command Interface
-  string command = "h"; //command option for interface
+
+  //TODO: Instantiate Simulator classes
+
+  //Set up simulator command interface
+  string command = "h"; //interface switch statement selector
   int n = 1; // number of cycles or addresses modifier
   int d = 0; // cycle display interval modifier
-  
+
   while (command != "q") {
     //Get the next command
     cin >> command;
-    
+
     //Simulate
     if (command == "s") {
       cin >> n >> d;
@@ -133,6 +127,6 @@ int main(int argc, char** argv)
       interface(command)
     }
   }
-  
+
   return 0;
 }
--- register.cpp
+++ register.cpp
@@ -2,5 +2,26 @@
 File:     register.cpp
 Authors:  Matthew Cole <mcole8@binghamton.edu>
           Brian Gracin <bgracin1@binghamton.edu>
-Description: Contains the Register class, which simulates operation of a register file.
+Description: Contains the Registers class, which simulates operation of a register file.
 */
+
+class Registers
+{
+private:
+  std::map<std::string,short> values;
+  std::map<std::string,bool> valids;
+public:
+  RegisterFile(); //initialize the RF with all registers/flags set to zero
+
+  void initialize(); //reset the RF with all registers/flags set to zero
+  void display(); //display the contents of the registers/flags
+
+  //methods for interacting with values map
+  void write(string register, int value); //sets values[register] = value
+  int read(string register); //return values[register]
+
+  //methods for interacting with valid map
+  void setValid(string register, bool valid); //set valids[register] = valid
+  void flipValid(string register); //set valids[register] = !valids[register]
+  bool getValid(string register); //return valids[register]
+}
