f2affb9a6d5d761559013f055d3193e3cc7dceef
Matthew Cole <mcole8@binghamton.edu>
2016-11-12 11:57:20 -0500

    Further cleanup to main
    Finished removing functionality not required by problem description.
    Main is minimal now.

--- main.cpp
+++ main.cpp
@@ -7,16 +7,32 @@ Description: Driver for apex-sim. Contains functions controlling simulator high-
 #include <iostream>
 #include <string.h>
 
+#define VERBOSE 1
+
 using namespace std;
 
 //Simulator variables with external linkage
 extern int cycle = 0; //simulator's current cycle
 extern const char* instFile; //instruction input file
 
+//Display an interface help message
+void help()
+{
+  cout << "[i]   Initialize the simulator state" << endl;
+  cout << "[s n] Simulate the number of cycles specified by <n>," << endl;
+  cout << "      displaying contents every <disp_cycles> cycles" << endl;
+  cout << "[d n] Display the contents of each stage in the pipeline," << endl;
+  cout << "      registers/flags, and the first <num_addr> memory " <<endl;
+  cout << "      locations containing data" << endl;
+  cout << "[q]   Quit the simulator" << endl;
+  cout << "[h]   Help message (repeat this information)" << endl;
+}
+
 // Initialize the simulator to a known state.
 void initialize()
 {
-  cout << "initialize()" << endl;
+  if (VERBOSE)
+    cout << "Initializing ... " << endl;
   //TODO Call to data::initialize()
   //TODO Call to registers::initialize()
   //TODO Call to cpu::initialize()
@@ -25,108 +41,88 @@ void initialize()
 // Display the simulator internal state.
 void display(int start=0, int stop=0)
 {
-  cout << "display()" << endl;
+  if (VERBOSE)
+    cout << "Displaying simulator state ... " << endl;
   //TODO Call to cpu::display()
   //TODO Call to registers::display()
   //TODO Call to data::display(start,stop)
   //TODO Call to code::display()
 }
 
-// Simulate the operation of the system for <num_cycles> or until a HALT instruction is encountered. Optionally display the simulator internal state every <disp_cycles> cycles. If <disp_cycles> is zero, the internal state is not displayed.
-void simulate(int num_cycles, unsigned int disp_cycles=0)
+// Simulate the operation of the system for <num_cycles>, or until a HALT
+//instruction is encountered, or until an error occurs in simulation.
+//Return the current cycle number after simulation pauses or halts.
+int simulate(int num_cycles)
 {
-  int start = cycle;
-  int stop = cycle + num_cycles;
-
-  for (int c = start; c < stop; c++)
+  for (int c = cycle; c < cycle + num_cycles; c++)
   {
     //Perform one cycle of simulation
-    cout << "simulate()" << endl;
-    //if(!cpu::simulate()) // simulate returns 0 if execution should not continue (EOF or HALT encountered)
-    //  break;
+    if (VERBOSE)
+      cout << "Simulating cycle " << cycle << " ..." << endl;
 
-    //Determine whether the machine state should be displayed
-    if (display && ((c - start) % display == 0))
-      display();
+    //cpu::simulate() returns 0 if execution should not continue
+    //(EOF, HALT or exeception encountered)
+    //if(!cpu::simulate())
+    //  break;
 
     //Cycle complete, increment the global cycle counter
     cycle++;
   }
-}
 
-// Provide the simulator's command interface
-void interface(string command, int n=1, int d=0)
-{
-  switch (command) {
-    case "i":
-      initialize();
-      break;
-    case "s":
-      simulate(n, d);
-      break;
-    case "d":
-      display(n);
-      break;
-    case "q":
-      break;
-    case "h": //falls through to default (which prints the help message)
-    default:
-      cout << "[i]                        (I)nitialize the simulator state" << endl;
-      cout << "[s num_cycles disp_cycles] (S)imulate the number of cycles specified by <n>," << endl;
-      cout << "                               displaying contents every <disp_cycles> cycles" << endl;
-      cout << "[d num_addr]               (D)isplay the contents of each stage in the pipeline," << endl;
-      cout << "                               registers/flags, and the first <num_addr> memory " <<endl;
-      cout << "                               locations containing data" << endl;
-      cout << "[q]                        (Q)uit the simulator" << endl;
-      cout << "[h]                        (H)elp message (repeat this information)" << endl;
-      break;
-  }
+  return cycle;
 }
 
 int main(int argc, char** argv)
 {
   //Command line program options handling
   //Check if user needs a usage message
-  if (argc == 1) {
-    cout << "USAGE: apex-sim [-h|--help] <instructions file>" << endl;
+  if (argc != 2) {
+    cout << "USAGE: apex-sim <instructions file>" << endl;
     exit(0);
   }
-  else if (!strcmp(argv[1], "-h") || !strcmp(argv[1],"--help")) {
-        cout << "USAGE: apex-sim [-h|--help] <instructions file>" << endl;
-        exit(0);
-      }
-    }
+  else {
+    //User didn't need a usage message. Capture filenames
+    instFile = (const char*) argv[1];
   }
 
-  //User didn't need a usage message. Capture filenames
-  instFile = (const char*) argv[1];
+  //TODO Perform entry actions?
 
-  //TODO: Instantiate Simulator classes
+  //TODO Instantiate Simulator classes
 
   //Set up simulator command interface
   string command = "h"; //interface switch statement selector
   int n = 1; // number of cycles or addresses modifier
-  int d = 0; // cycle display interval modifier
 
+  /****** USER INTERFACE ******/
   while (command != "q") {
-    //Get the next command
+    //On first execution of the interface, display the help message
+    if (cycle == 0)
+      help();
+
+    //Get the next command. If command takes the n parameter, ingest it also
     cin >> command;
-
-    //Simulate
-    if (command == "s") {
-      cin >> n >> d;
-      interface(command, n, d);
-    }
-    //Display
-    else if (command == "d") {
+    if (command == "s" || command == "d")
       cin >> n;
-      interface(command, n);
-    }
-    //Initialize, Quit, or Help
-    else {
-      interface(command)
-    }
-  }
 
+    //Perform appropriate action based on the command / parameter
+    switch (command) {
+      case "i":
+        initialize();
+        break;
+      case "s":
+        simulate(n);
+        break;
+      case "d":
+        display(n);
+        break;
+      case "q": //quitting causes no action except to conclude this function
+        break;
+      case "h": //Falls through to default (which prints the help message)
+      default: //Input wasn't recognized
+        help();
+        break;
+  } // End User Interface
+
+  //TODO Perform exiting actions?
   return 0;
 }
