6b97c3fb3c63317d15bd2555e91e30d3e699f5de
Matthew Cole <mcole8@binghamton.edu>
2016-11-12 17:39:42 -0500

    Input files and documentation
    Added a second test input file, created documentation/inputs subfolders
    and moved all documentation to the docs folder.
    
    Minor changes to register.* to align with design goals.

--- register.cpp
+++ register.cpp
@@ -5,29 +5,55 @@ Authors:  Matthew Cole <mcole8@binghamton.edu>
 Description: Contains the Registers class, which simulates operation of a register file.
 */
 
+#include "register.h"
+
 Registers::Registers(){
   this->initialize();
 }
 
-//initialize the RF with all registers/flags set to zero
+//Initialize the RF with all registers/flags set to zero
+//and all validities set to true
 void Registers::initialize(){
-  int r;
   std::string reg_name;
 
-  //Set register values and valids
-  for (r=0; r<=15; r++){
+  //Set general purpose register values and valids
+  for (int r=0; r<NUM_REGS; r++){
     reg_name = "R" + std::to_string(r);
-    this.values[reg_name] = 0;
+    this.reg_file[reg_name] = std::make_tuple(0, true)
   }
 
-  //Set flag values and valids
-  this.values["Z"] = 0;
-  this.valids["Z"] = true;
+  //Set flag and special register values and valids
+  this.reg_file["X"] = std::make_tuple(0, true);
+  this.reg_file["Z"] = std::make_tuple(0, true);
 }
 
-//display the contents of the registers/flags
+//Display the contents of the registers/flags
 void Registers::display(){
-  for (auto it = reg_file.begin(); it != reg_file.end(); ++it) {
-    std::cout << it->first << ": " << it->second << '\n';
+  std::string myname = "";
+  int myvalue = 0;
+  bool myvalid = true;
+
+  std::cout << "Register File and Flags:" << endl;
+  for (auto it = this.reg_file.begin(); it != this.reg_file.end(); ++it) {
+    myname = it->first;
+    std::tie(myvalue, myvalid) = it->second;
+    std::cout << myname << ": " << myvalue << "," << myvalid << std::endl;
   }
+}
+
+//Set reg_file[register] = (value,valid)
+void write(std::string register, int value, bool valid){
+
+}
+
+//Get reg_file[register] value
+//WARNING: Does NOT perform any membership test
+int read(std::string register){
+
+}
+
+//Check reg_file[register] validity
+//WARNING: Does not perform any membership test
+bool isValid(std::string register){
+
 }
--- register.h
+++ register.h
@@ -12,20 +12,25 @@ Description: header file for register.cpp
 #ifndef REGISTER_H
 #define REGISTER_H
 
+#define NUM_REGS 16 //number of registers in this ISA
+
 class Registers
 {
 private:
-  std::map<std::string,int> reg_file;
+  //register := {string name:(int value, bool valid)}
+  std::map<std::string, std::tuple<int,bool>> reg_file;
 
 public:
   Registers(); //initialize the RF with all registers/flags set to zero
 
+  //methods for simulator's interface control
   void initialize(); //reset the RF with all registers/flags set to zero
   void display(); //display the contents of the registers/flags
 
-  //methods for interacting with values map
-  void write(string register, int value, bool valid); //sets values[register] = (value,valid)
-  int read(string register); //return values[register]
+  //methods for interacting with the register file map
+  void write(std::string register, int value, bool valid); //sets reg_file[register] = (value,valid)
+  int read(std::string register); //get reg_file[register] value
+  bool isValid(std::string register); //get reg_file[register] validity
 };
 
 #endif
