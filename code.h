b3d6a9c11ca3935f52f221538fd998b03032ff6d
gracinb <gracinbd@gmail.com>
2016-11-12 15:20:52 -0500

    Pushing correct code.h

--- code.h
+++ code.h
@@ -2,14 +2,13 @@
 File:     mmu.h
 Authors:  Matthew Cole <mcole8@binghamton.edu>
           Brian Gracin <bgracin1@binghamton.edu>
-Description: Header for mmu.cpp
+Description: Header for code.cpp
 */
+#ifndef CODE_H
+#define CODE_H
 
+class Code{
+	void readCode();
 
-struct instruction_t {
-  int pc; // the Program Counter value of this instruction
-  std::string opcode; //the string (e.g. ADD) representing this instruction
-  std::vector<std::string> operands; //a vector of strings representing operands
-}
-
-int littoi(string literal); //Convert a literal string to an integer value 
+};
+#endif
