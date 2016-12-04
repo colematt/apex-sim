6c84f3fd6a4e6537d7ddde14c0f2fb00d1da95f2
Matthew Cole <mcole8@binghamton.edu>
2016-12-04 15:58:33 -0500

    Added stats class files, header and updated Makefile

---
+++ stats.cpp
@@ -0,0 +1,8 @@
+/* FILE INFORMATION
+File:     stats.cpp
+Authors:  Matthew Cole <mcole8@binghamton.edu>
+          Brian Gracin <bgracin1@binghamton.edu>
+Description: Provides statistics tracking and computation for apex-sim
+*/
+
+#include "stats.h"
---
+++ stats.h
@@ -0,0 +1,12 @@
+/* FILE INFORMATION
+File:     stats.h
+Authors:  Matthew Cole <mcole8@binghamton.edu>
+          Brian Gracin <bgracin1@binghamton.edu>
+Description: Provides statistics tracking and computation for apex-sim
+*/
+
+#ifndef STATS_H
+#define STATS_H
+
+
+#endif
