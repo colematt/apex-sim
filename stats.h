d96c772548092f612c306cc6ffd3c7425dfffcd4
Matthew Cole <mcole8@binghamton.edu>
2016-12-05 01:26:49 -0500

    Statistics Module
    Added Stats class and function implementations.

--- stats.h
+++ stats.h
@@ -8,5 +8,38 @@ Description: Provides statistics tracking and computation for apex-sim
 #ifndef STATS_H
 #define STATS_H
 
+class Stats {
+private:
+
+public:
+
+  //Data members
+  unsigned int cycle;           // number of cycles simulated
+  unsigned int dispatched;      // number of instructions dispatched to IQ
+  unsigned int no_dispatched;   // number of cycles with no dispatch to IQ
+  unsigned int issued;          // number of instructions issued to any FU
+  unsigned int no_issued;       // number of cycles with no instructions issued
+  unsigned int resolved;        // number of instructions resolved from B FU
+  unsigned int committed;       // number of instructions committed from ROB
+  unsigned int load_committed;  // number of LOAD instructions committed
+  unsigned int store_committed; // number of STORE instructions committed
+
+  Stats ();
+  ~stats ();
+
+  // Reset all stats counters
+  void initialize();
+
+  // Display all stats
+  void display();
+
+  // Statistics getters
+  double ipc(); // Instructions per Cycle
+  unsigned int stalled();       // Number of cycles dispatch stalled
+  unsigned int no_issue();      // Number of cycles with no issue to an FU
+  unsigned int load_commit();   // Number of LOAD instructions committed
+  unsigned int store_commit();  // Number of STORE instructions committed
+
+};
 
 #endif
