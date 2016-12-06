3b0d6906179357ee5bd581fd87ef272d7c1e94ef
gracinb <gracinbd@gmail.com>
2016-12-05 20:46:03 -0500

    Updating register class files
    Converting register class into URF

--- srcs/register.cpp
+++ srcs/register.cpp
@@ -20,10 +20,14 @@ Registers::~Registers(){
 void Registers::initialize(){
   std::string reg_name;
 
+  //Purge reg_file
+  this->reg_file.clear();
+
   //Set general purpose register values and valids
-  for (int r=0; r<NUM_REGS; r++){
-    reg_name = "R" + std::to_string(r);
+  for (int r=0; r<num_reg; r++){
+    reg_name = "P" + std::to_string(r);
     this->reg_file[reg_name] = std::make_tuple(0, true);
+    this->free_list.push(reg_name); //Populate free list
   }
 
   //Set flag and special register values and valids
@@ -44,6 +48,51 @@ void Registers::display(){
   }
 }
 
+//Create new instance of an R reg
+std::string Registers::getRenamed(std::string archReg){
+  std::string physReg = this->free_list.pop();
+  std::string regHolder;
+
+  this->front_end[physReg] = archReg;
+
+  //Only update back_end if entry exists
+  auto it = this->back_end.find(rReg);
+  if (it != this->back_end.end()) {
+    regHolder = std::get<0>(itt->second;
+    this->back_end[rReg] = std::make_tuple(regHolder, false); //Set current to false
+  } else 
+
+  return physReg;
+}
+
+void Registers::commit(std::string pReg){
+  std::string rReg;
+  std::string prevReg;
+
+  //Get R reg matched to P reg in front end list
+  auto it = this->front_end.find(pReg);
+  if (it != this->front_end.end()) {
+    rReg = it->second;
+  } else {
+    std::string what_arg = reg + " is not a valid register";
+    throw std::invalid_argument(what_arg);
+  }
+
+  //Get P reg matched to R reg in back end list
+  auto itt = this->back_end.find(rReg);
+  if (itt != this->back_end.end()) {
+    prevReg = std::get<0>(itt->second;
+    this->back_end[rReg] = std::make_tuple(pReg, true); //Set new P reg value
+  } else {
+    std::string what_arg = reg + " is not a valid register";
+    throw std::invalid_argument(what_arg);
+  }
+
+  this->front_end[pReg] = ""; //Remove mapped R reg
+  this->free_list.push(pReg); //Push P reg back into free list
+
+}
+
 //Set reg_file[register] = (value,valid)
 //Throws std::invalid_argument exception if register doesn't exist
 void Registers::write(std::string reg, int value, bool valid){
@@ -58,7 +107,7 @@ void Registers::write(std::string reg, int value, bool valid){
 
 //Get reg_file[register] value
 //Throws std::invalid_argument exception if register doesn't exist
-int Registers::read(std::string reg){
+int Registers::physRead(std::string reg){
   int myvalue;
 
   auto it = this->reg_file.find(reg);
@@ -73,7 +122,7 @@ int Registers::read(std::string reg){
 
 //Check reg_file[register] validity
 //Throws std::invalid_argument exception if register doesn't exist
-bool Registers::isValid(std::string reg){
+bool Registers::physIsValid(std::string reg){
   bool myvalid;
   auto it = this->reg_file.find(reg);
   if (it != this->reg_file.end()) {
@@ -84,4 +133,47 @@ bool Registers::isValid(std::string reg){
   }
 
   return myvalid;
+}
+
+//Translate R reg to P reg to get value
+//Throws std::invalid_argument exception if mapping doesn't exist
+int Registers::read(std::string reg){
+  int myvalue;
+  std::string myReg
+
+  auto it = this->back_end.find(reg);
+  if (it != this->back_end.end()) {
+    myReg = std::get<0>(it->second);
+  } else {
+    std::string what_arg = reg + " is not a valid register";
+    throw std::invalid_argument(what_arg);
+  }
+
+  myvalue = physRead(myReg);
+
+  return myvalue;
+}
+
+//Translate R reg to P reg to get validity
+//Throws std::invalid_argument exception if mapping doesn't exist
+bool Registers::isValid(std::string reg){
+  bool myvalid;
+  std::string myReg
+
+  auto it = this->back_end.find(reg);
+  if (it != this->back_end.end()) {
+    myReg = std::get<0>(it->second);
+  } else {
+    std::string what_arg = reg + " is not a valid register";
+    throw std::invalid_argument(what_arg);
+  }
+
+  myvalid = physIsValid(myReg);
+
+  return myvalid;
+}
+
+void Registers::setNumReg(int num){
+  if (num > 0)
+    num_reg = 0;
 }
--- srcs/register.h
+++ srcs/register.h
@@ -15,27 +15,52 @@ Description: header file for register.cpp
 #ifndef REGISTER_H
 #define REGISTER_H
 
-#define NUM_REGS 16 //number of registers in this ISA
-
 class Registers
 {
 private:
   //register := {string name:(int value, bool valid)}
   std::map<std::string, std::tuple<int,bool>> reg_file;
 
+  //map <pReg, rReg>
+  std::map<std::string, std::string> front_end;
+
+  //maps <rReg, tuple<pReg, most recent bit>>
+  std::map<std::string, std::tuple<string,bool>> back_end;
+
+  //holds list of physical registers that are ready to to be used
+  std::queue<std::string> free_list;
+
+  //Number of physical registers, default=32
+  int num_reg = 32;
+
 public:
   Registers(); //initialize the RF with all registers/flags set to zero
 
-  ~Registers(); //Deconstructor
+  ~Registers(); //deconstructor
 
   //methods for simulator's interface control
   void initialize(); //reset the RF with all registers/flags set to zero
   void display(); //display the contents of the registers/flags
 
+  //create instance of R reg
+  std::string getRenamed(std::string);
+
+  //TODO create function that updates back_end list
+  void commit(std::string pReg, std::string rReg);
+  //TODO create function to release physical registers
+  //TODO
+
   //methods for interacting with the register file map
-  void write(std::string register, int value, bool valid); //sets reg_file[register] = (value,valid)
-  int read(std::string register); //get reg_file[register] value
-  bool isValid(std::string register); //get reg_file[register] validity
+  void write(std::string reg, int value, bool valid); //sets reg_file[register] = (value,valid)
+  int physRead(std::string reg); //get reg_file[register] value
+  bool physIsValid(std::string reg); //get reg_file[register] validity
+
+  //void write(std::string reg, int value, bool valid); //sets reg_file[register] = (value,valid)
+  int read(std::string reg); //take R reg, get P reg, call to get value
+  bool isValid(std::string reg); //take R reg, get P reg, call to get validity
+
+  //method to set the number of physical registers in processor
+  void setNumReg(int num); //sets phy_reg = num (default = 32)
 };
 
 #endif
